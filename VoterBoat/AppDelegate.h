d26cce7988d87aa9012904caeca75c4121dda561
SlayterDev <BradleySlayter@my.unt.edu>
2014-09-18 10:33:12 -0500

    Initial Commit

---
+++ VoterBoat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VoterBoat
+//
+//  Created by Bradley Slayter on 9/18/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VoterBoat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VoterBoat
+//
+//  Created by Bradley Slayter on 9/18/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
