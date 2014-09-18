12b3707cfd4171c5aba09d86433d496eaf499256
SlayterDev <BradleySlayter@my.unt.edu>
2014-09-18 10:33:12 -0500

    Initial Commit

---
+++ VoterBoat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VoterBoat
+//
+//  Created by Bradley Slayter on 9/18/14.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
